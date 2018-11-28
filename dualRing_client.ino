58fc438aa06d90876b5788fcdf559e0e59a197ce
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-11-28 10:22:01 -0700

    Menus and an alternate motion

--- dualRing_client.ino
+++ dualRing_client.ino
@@ -5,11 +5,25 @@
  */
 
 #include "DualRingLED.h"
+#include <SoftwareSerial.h>
 
 // Which pin to use for DualRingLED control
 #define LED_PIN    6
 
 DualRingLED myLights(LED_PIN);
+int delayTime;
+
+void moveLightsMod( void )
+{
+  static int phase=0;
+
+  if (phase % 3 == 0) myLights.rotateInnerClockwise();
+  if (phase % 2 == 0) myLights.rotateOuterClockwise();
+
+  phase++;
+  if (phase == 6) phase = 0;
+  
+}
 
 void moveLights( void )
 {
@@ -38,17 +52,63 @@ void initLights( void )
   myLights.fillAll(CRGB::Blue);
   myLights.innerLEDs[0] = CRGB::Green;
   myLights.outerLEDs[0] = CRGB::Red;
+
+  delayTime=100;
   
   myLights.setRunFunc(moveLights);
 }
 
+void printMenu( void )
+{
+  Serial.println("1 selects case-style motion");
+  Serial.println("2 selects modulus motion");
+}
+
+void userInput( void )
+{
+  char c;
+  
+  while (Serial.available())
+  {
+    c = Serial.read();
+
+    switch (c)
+    {
+      case '1':
+        myLights.setRunFunc(moveLights);
+        delayTime = 100;
+        Serial.println("case motion");
+      break;
+
+      case '2':
+        myLights.setRunFunc(moveLightsMod);
+        delayTime = 50;
+        Serial.println("mod motion");
+      break;
+
+      case '\n':
+        // do nothing with returns
+      break;
+      
+      default:
+        Serial.println("unrecognized command.");
+        printMenu();
+    }
+  }
+}
+
 void setup()
 {
+    Serial.begin(9600);
     myLights.begin();
+    
     initLights();
+
+    printMenu();
 }
 
 void loop()
 {
-    myLights.run(100);
+    userInput();
+    myLights.run(delayTime);
 }
