aba7c81305f43ac74619e28d19737f7d17dd7ff4
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-12-03 11:00:18 -0700

    clean up to gradient fill.

--- dualRing_client.ino
+++ dualRing_client.ino
@@ -13,10 +13,10 @@ DualRingLED myLights(LED_PIN);
 
 const TProgmemPalette16 my_palette PROGMEM =
 {
-  CRGB::Blue,
-  CRGB::Red,
-  CRGB::Yellow,
-  CRGB::Blue,
+  CRGB::Black,
+  CRGB::Black,
+  CRGB::Black,
+  CRGB::Black,
 
   CRGB::Blue,
   CRGB::Red,
@@ -35,7 +35,7 @@ const TProgmemPalette16 my_palette PROGMEM =
 
 };
 
-void waterfallGradFill( CRGB *ledArray, int num, CRGB head, CRGB tail)
+void waterfallGradFill( CRGB *ledArray, int num, CRGB startPos, CRGB endPos)
 {
   int i;  
   int mid;
@@ -44,7 +44,7 @@ void waterfallGradFill( CRGB *ledArray, int num, CRGB head, CRGB tail)
   
   // need to do this in two steps.
   // First, fill from the "zeroth" led to half way around (8 for inner, 12 for outer)
-  fill_gradient_RGB(ledArray, mid + 1, head, tail);
+  fill_gradient_RGB(ledArray, mid + 1, startPos, endPos);
 
   // now mirror the other half
   for (i=1; i<mid; i++)
@@ -53,123 +53,6 @@ void waterfallGradFill( CRGB *ledArray, int num, CRGB head, CRGB tail)
   }
 }
 
-void _waterfallMotionHelper( CRGB first, int numLEDs, CRGB *ledArray )
-{
-  int i;
-  int middleLED;
-
-  middleLED = numLEDs / 2;
-  
-    // Inner example...
-  // 0 goes to 1 
-  // 1 goes to 2 
-  /// ...
-  // 6 goes to 7
-  // and then on the other side:
-  // 15 goes to 14
-  // 14 goes to 13
-  // ...
-  // 9 goes to 8
-  // but you have to do all this backwards...7 shifts and one copy each for inner
-
-  // lower indexes
-  for (i = middleLED - 1; i > 0; i--)
-  {
-    ledArray[i] = ledArray[i - 1];
-  }
-
-  // upper indexes
-  for (i = middleLED; i < numLEDs - 1; i++)
-  {
-    ledArray[i] = ledArray[i+1];
-  }
-
-  ledArray[0] = first;
-  ledArray[numLEDs - 1] = first;
-
-}
-
-void innerWaterfallRotate( void )
-{
-  CRGB rotateColor;
-
-  rotateColor = myLights.innerLEDs[8];
-  _waterfallMotionHelper(rotateColor, DUAL_RING_NUM_INNER, myLights.innerLEDs);
-}
-
-void outerWaterfallRotate( void )
-{
-  CRGB rotateColor;
-
-  myLights.rotateOuterCounterClockwise();
-  
-  rotateColor = myLights.outerLEDs[12];
-  _waterfallMotionHelper(rotateColor, DUAL_RING_NUM_OUTER, myLights.outerLEDs);
-  
-  myLights.rotateOuterClockwise();
-}
-
-void innerWaterfallCycle( void )
-{
-  static uint8_t index=0;
-  CRGB color;
-
-  color = ColorFromPalette(my_palette, index);
-  _waterfallMotionHelper(color, DUAL_RING_NUM_INNER, myLights.innerLEDs);
-
-  index++;
-}
-
-void outerWaterfallCycle( void )
-{
-  static uint8_t index=0;
-  CRGB color;
-
-  color = ColorFromPalette(my_palette, index);
-  _waterfallMotionHelper(color, DUAL_RING_NUM_OUTER, myLights.outerLEDs);
-
-  index++;
-  
-}
-
-void moveLights( void )
-{
-  static int phase = 0;
-
-  if (phase % 3 == 0) innerWaterfallCycle();
-  if (phase % 2 == 0) outerWaterfallCycle();
-
-  phase++;
-  if (phase == 6) phase = 0;
-}
-
-void syncCycle( void )
-{
-  static uint8_t index=0;
-  CRGB color;
-
-
-  // 256 % 2 works, but 256 % 3 does not.  Gonna use 240...
-  if (index % 3 == 0) 
-  {
-     color = ColorFromPalette(my_palette, index);
-     _waterfallMotionHelper(color, DUAL_RING_NUM_INNER, myLights.innerLEDs);
-  }
-
-  if (index % 2 == 0)
-  {
-     color = ColorFromPalette(my_palette, index);
-     myLights.rotateOuterCounterClockwise();
-     _waterfallMotionHelper(color, DUAL_RING_NUM_OUTER, myLights.outerLEDs);
-     myLights.rotateOuterClockwise();
-  }
-
-  index++;
-  if (index == 240) index = 0;
-
-}
-
-
 void pulse( void )
 {
   static uint8_t start_index = 0;
@@ -177,18 +60,16 @@ void pulse( void )
   CRGB head_color;
   
   tail_color = ColorFromPalette(my_palette, start_index);
-  head_color = ColorFromPalette(my_palette, start_index + 12);
+  head_color = ColorFromPalette(my_palette, start_index + 8);
   
-  waterfallGradFill(myLights.innerLEDs, 16, tail_color, head_color); 
-  waterfallGradFill(myLights.outerLEDs, 24, tail_color, head_color);
+  waterfallGradFill(myLights.innerLEDs, 16, head_color, tail_color); 
+  waterfallGradFill(myLights.outerLEDs, 24, head_color, tail_color);
 
   start_index++;
 }
 
 void initLights( void )
 {
-  // waterfallGradFill(myLights.innerLEDs, 16, CRGB::Blue, CRGB::Red); 
-  // waterfallGradFill(myLights.outerLEDs, 24, CRGB::Blue, CRGB::Red);
   myLights.setRunFunc(pulse);
   
 }
@@ -201,5 +82,5 @@ void setup()
 
 void loop()
 {
-    myLights.run(25);
+    myLights.run(100);
 }
