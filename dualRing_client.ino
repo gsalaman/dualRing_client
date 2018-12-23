85d79ca877017fad6d45c1f221940d301d8afda7
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-12-23 07:05:38 -0700

    pattern selection in menus

--- dualRing_client.ino
+++ dualRing_client.ino
@@ -1,9 +1,9 @@
 /*===========================================================
  * Dual Ring LED Client 
  * 
- * Excercise 7:  Waterfall pattern, delays, and simple menus.
+ * Excercise 8:  Expanding on menus.
  */
-
+#include "FastLED.h"
 #include "DualRingLED.h"
 #include <SoftwareSerial.h>
 
@@ -12,28 +12,62 @@
 
 DualRingLED myLights(LED_PIN);
 
-#define MIN_DELAY 10
-#define MAX_DELAY 200
+#define MIN_LOOP_DELAY 10
+#define MAX_LOOP_DELAY 200
 
 int loop_delay=100;
 
-void moveLights( void )
+
+/*=============================================
+ * Pattern functions
+ *=============================================*/
+
+ /*  Pattern: Blackout.  Turn off all leds. */
+void init_blackout( void )
+{
+  myLights.fillAll(CRGB::Black);
+  myLights.setRunFunc(NULL);
+  Serial.println("Blackout selected");
+}
+
+/*  Pattern:  Waterfall.  From example 7, cascading colors */
+void move_waterfall( void )
 {
   myLights.waterfall();
 }
 
-void initLights( void )
+void init_waterfall( void )
 {
-  loop_delay = 70;
-  myLights.setRunFunc(moveLights);
+    myLights.setRunFunc(move_waterfall);
+    Serial.println("Waterfall selected");
 }
 
+/* Pattern:  Clockwise unsynced */
+void move_clockwise_unsynced( void )
+{
+  myLights.rotateInnerClockwise();
+  myLights.rotateOuterClockwise();
+}
+
+void init_clockwise_unsynced( void )
+{
+  myLights.makeInnerClockwiseStreak(6, CRGB::Blue, CRGB::Red);
+  myLights.makeOuterBump(4, CRGB::Blue, CRGB::Red);
+  myLights.setRunFunc(move_clockwise_unsynced);
+  
+}
+
+/*=============================================
+ * MENU functions
+ *=============================================*/
 void print_menu( void )
 {
   Serial.println("+ to slow down, - to speed up");
   Serial.print("Current delay: ");
   Serial.println(loop_delay);
-  
+  Serial.println("0 to select Blackout pattern");
+  Serial.println("1 to select Waterfall pattern");
+  Serial.println("2 to select clockwise unsynced");
 }
 
 void user_input( void )
@@ -47,17 +81,29 @@ void user_input( void )
     switch (c)
     {
       case '+':
-        if (loop_delay < MAX_DELAY) loop_delay += 10;
+        if (loop_delay < MAX_LOOP_DELAY) loop_delay += 10;
         Serial.print("Current delay: ");
         Serial.println(loop_delay);   
       break;
 
       case '-':
-        if (loop_delay > MIN_DELAY) loop_delay -= 10;
+        if (loop_delay > MIN_LOOP_DELAY) loop_delay -= 10;
         Serial.print("Current delay: ");
         Serial.println(loop_delay);   
       break;
 
+      case '0':
+        init_blackout();
+      break;
+
+      case '1':
+        init_waterfall();
+      break;
+
+      case '2':
+        init_clockwise_unsynced();
+      break;
+        
       case '\n':
         //do nothing
       break;
@@ -70,14 +116,14 @@ void user_input( void )
   }
 }
 
+/*============================================================
+ * Main Functions
+ *===========================================================*/
 void setup()
 {
     Serial.begin(9600);
-    
     myLights.begin();
-    
-    initLights();
-
+    init_waterfall();
     print_menu();
 
 }
